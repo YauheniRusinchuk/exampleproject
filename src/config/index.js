70f6ccfc701cd3b63fce561cf2a79a7a63e292d8
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2018-04-25 16:58:26 +0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 
 const config = {
-    apiKey: "AIzaSyAsVJXNlh4mNZhghmCMG1KvzjKAxjhFupg",
+    apiKey: "AIzaSyefaAsVJXNlh4mNZhghmCMG1KvzjKAdqdqdxjhFupg",
     authDomain: "myproject-e50f0.firebaseapp.com",
-    databaseURL: "https://myproject-e50f0.firebaseio.com",
-    projectId: "myproject-e50f0",
+    databaseURL: "https://myprojedact-e50f0.firebaseio.com",
+    projectId: "myproject-e50fd0",
     storageBucket: "",
-    messagingSenderId: "215754326167"
+    messagingSenderId: "21572554326167"
   };
 
 if(!firebase.apps.length) {
